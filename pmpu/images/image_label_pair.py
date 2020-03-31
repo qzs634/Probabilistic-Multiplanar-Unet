63478386fad04edcd36a5fdaa342a2c827983304
Niklas <nmagnussen@hotmail.com>
2020-03-31 16:55:09 +0200

    pillet ved images

--- pmpu/images/image_label_pair.py
+++ pmpu/images/image_label_pair.py
@@ -4,12 +4,14 @@ import numpy as np
 
 
 class ImageLabelPair():
-
+    """
+    ImageLabelPair:
+    - Stores images and corresponding labels, used for making batches.
+    """
     def __init__(self, image, label):
         self.image = image
         self.label = label
         if self.image.shape != self.label.shape:
             print("Mismath in image and label shapes - image shape: {}, label shape: {}".format(
-                self.image.shape, self.label.shape
-            ))
+                self.image.shape, self.label.shape))
         self.dims = self.image.shape
--- pmpu/main.py
+++ pmpu/main.py
@@ -3,13 +3,13 @@ from preprocessing.slicer import Slicer
 
 image_loader = ImageLoader("/home/bib/Documents/DIKU/Bachelor/data_folder/train")
 slicer = Slicer(image_loader,
-                out_path="/home/bib/Documents/DIKU/Bachelor/Pytorch-UNet/testdata",
+                out_path="/home/bib/Documents/DIKU/Bachelor/Pytorch-UNet/data",
                 out_image_path="imgs",
                 out_label_path="masks")
 
-num_batches = 1
+num_batches = 40
 for i in range(num_batches):
-    batch = slicer.make_batch(8)
+    batch = slicer.make_batch(10)
     slicer.save_batch_to_folder(batch)
     print("saved batch no. ", i)
 
