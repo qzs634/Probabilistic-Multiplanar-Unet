c0d9936efe5db8dc830f147b334378abaf724a2b
biscuitbib <nmagnussen@hotmail.com>
2020-04-12 08:33:36 +0200

    ny Pytorch-unet

--- pmpu/main.py
+++ pmpu/main.py
@@ -10,9 +10,13 @@ slicer = Slicer(image_loader,
                 out_image_path="imgs",
                 out_label_path="masks")
 
-num_batches = 40
+
+num_batches = 1
 for i in range(num_batches):
     batch = slicer.make_batch(10)
     slicer.save_batch_to_folder(batch)
     print("saved batch no. ", i)
 
+
+
+#slicer.make_all_pairs()
